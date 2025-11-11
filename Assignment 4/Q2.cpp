2f21a9c5bf34bfc00bdecc257c5a6ac440ea7925
Saptak-Patra <death.exp69@icloud.com>
2025-11-11 23:16:51 +0530

    Update Q2.cpp

--- Assignment 4/Q2.cpp
+++ Assignment 4/Q2.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+class num {
+public:
+    int n1, n2;
+
+    void accept() {
+        cout << "Enter first number: ";
+        cin >> n1;
+        cout << "Enter second number: ";
+        cin >> n2;
+    }
+
+    void display() {
+        cout << "Num 1: " << n1 << endl;
+        cout << "Num 2: " << n2 << endl;
+    }
+
+    friend void swap(num &nu);
+};
+
+void swap(num &nu) {
+    int temp = nu.n1;
+    nu.n1 = nu.n2;
+    nu.n2 = temp;
+    cout << "Swapped:" << endl;
+    cout << "First: " << nu.n1 << endl;
+    cout << "Second: " << nu.n2 << endl;
+}
+
+int main() {
+    num nu;
+    nu.accept();
+    nu.display();
+    swap(nu);
+    return 0;
+}
